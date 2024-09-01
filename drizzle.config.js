6d7a4a84642355d9103910f44f35476dcd055b45
telloom <157322276+telloom@users.noreply.github.com>
2024-09-01 10:20:41 -0400

    Save current changes

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,8 @@
 import * as dotenv from 'dotenv';
-import * as fs from 'fs';
-import * as path from 'path';
+import { defineConfig } from 'drizzle-kit';
 import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
 
 dotenv.config();
 
@@ -10,18 +11,17 @@ const __dirname = path.dirname(__filename);
 
 console.log('Current working directory:', process.cwd());
 console.log('Database URL:', process.env.DATABASE_URL);
-console.log('Schema path:', path.resolve('./db/schema'));
+console.log('Schema path:', path.resolve(__dirname, './db/schema'));
 
 // List schema files
-const schemaDir = path.resolve('./db/schema');
+const schemaDir = path.resolve(__dirname, './db/schema');
 console.log('Schema files:', fs.readdirSync(schemaDir));
 
-/** @type { import("drizzle-kit").Config } */
-export default {
+export default defineConfig({
   schema: './db/schema/*',
   out: './db/migrations',
   driver: 'pg',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL,
   },
-};
+});
