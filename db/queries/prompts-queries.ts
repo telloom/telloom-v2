ee5b711c045887272d24dc35cab7b79621431cfa
telloom <157322276+telloom@users.noreply.github.com>
2024-09-02 11:38:07 -0400

    save commit

--- db/queries/prompts-queries.ts
+++ db/queries/prompts-queries.ts
@@ -4,29 +4,29 @@ import { sql } from '@vercel/postgres';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import * as schema from "../schema";
 import { eq } from "drizzle-orm";
-import { InsertPrompt, SelectPrompt } from "../schema/prompts";
+import { InsertPrompt, SelectPrompt } from "../schema/prompts_primary";
 
 // Create a typed database instance
 const typedDb = drizzle(sql, { schema });
 
 export const createPrompt = async (data: InsertPrompt) => {
-  return typedDb.insert(schema.promptsTable).values(data).returning();
+  return typedDb.insert(schema.promptsPrimaryTable).values(data).returning();
 };
 
 export const getPromptById = async (id: bigint) => {
-  return typedDb.query.promptsTable.findFirst({
-    where: eq(schema.promptsTable.id, Number(id)),
+  return typedDb.query.promptsPrimaryTable.findFirst({
+    where: eq(schema.promptsPrimaryTable.id, Number(id)),
   });
 };
 
 export const getAllPrompts = async () => {
-  return typedDb.query.promptsTable.findMany();
+  return typedDb.query.promptsPrimaryTable.findMany();
 };
 
 export const updatePrompt = async (id: bigint, data: Partial<InsertPrompt>) => {
-  return typedDb.update(schema.promptsTable).set(data).where(eq(schema.promptsTable.id, Number(id))).returning();
+  return typedDb.update(schema.promptsPrimaryTable).set(data).where(eq(schema.promptsPrimaryTable.id, Number(id))).returning();
 };
 
 export const deletePrompt = async (id: bigint) => {
-  return typedDb.delete(schema.promptsTable).where(eq(schema.promptsTable.id, Number(id)));
+  return typedDb.delete(schema.promptsPrimaryTable).where(eq(schema.promptsPrimaryTable.id, Number(id)));
 };
