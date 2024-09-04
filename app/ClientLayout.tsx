b0fe2d198689f001713768a09f9937b69b4e9569
telloom <157322276+telloom@users.noreply.github.com>
2024-09-04 13:44:39 -0400

    Update project files

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useState } from 'react'
-import { Session, createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 
 export default function ClientLayout({
